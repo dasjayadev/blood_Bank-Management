1403a58a2f229d3f05a62f5b25c4306a8a6c28d8
dasjayadev <jayadev.das2581@gmail.com>
2024-07-08 14:19:05 +0530

    lazy loading pending

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -3,27 +3,30 @@ import Form from "../../component/shared/Form/Form";
 
 function Login() {
   return (
-    <div className="container-fluid vh-100">
-      <div className="row h-100 g-0">
-        <div className="col-md-6 d-none d-md-block p-0">
-          <img
-            src="./assets/blood cover.jpg"
-            alt="blood_bank_login"
-            className="img-fluid w-100 h-100"
-          />
-        </div>
-        <div className="col-md-6 d-flex align-items-center justify-content-center bg-light">
-          <div className="p-5 shadow rounded">
-            <Form
-              role=""
-              submitText="Submit"
-              formTitle="Login"
-              formType="login"
+    <>
+      <div className="container-fluid vh-100">
+        <div className="row h-100 g-0">
+          <div className="col-md-6 d-none d-md-block p-0">
+            <img
+              src="./assets/blood cover.jpg"
+              alt="blood_bank_login"
+              className="img-fluid w-100 h-100"
+              loading="lazy"
             />
+          </div>
+          <div className="col-md-6 d-flex align-items-center justify-content-center bg-light">
+            <div className="p-5 shadow rounded">
+              <Form
+                role=""
+                submitText="Submit"
+                formTitle="Login"
+                formType="login"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
